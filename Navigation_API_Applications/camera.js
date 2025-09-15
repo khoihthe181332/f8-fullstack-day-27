cae20b0401a65b2f480ac8c0c8846abe426db68a
khoihthe181332 <hatuankhoi2004@gmail.com>
2025-09-16 00:03:52 +0700

    Hoàn thiện bài camera-capture

--- Navigation_API_Applications/camera.js
+++ Navigation_API_Applications/camera.js
@@ -5,13 +5,23 @@ const startCameraBtn = $('#startCamera');
 const stopCameraBtn = $('#stopCamera');
 const takePhotoBtn = $('.btn-capture');
 const downloadBtn = $('.btn-download');
+const canvas = $("#photoCanvas");
+const imgPhoto = $("#capturedPhoto");
+const photoPlaceholder = $("#photoPlaceholder");
 
 let stream = null;
 const video = $("#videoStream");
 
+// Start Camera
 startCameraBtn.addEventListener('click', async () => {
     try {
-        stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        stream = await navigator.mediaDevices.getUserMedia({
+            video: {
+                width: { ideal: 1280 },
+                height: { ideal: 720 }
+            },
+            audio: false
+        });
         video.srcObject = stream;
         disableBtn(stream);
     } catch (err) {
@@ -19,6 +29,7 @@ startCameraBtn.addEventListener('click', async () => {
     }
 });
 
+// Stop Camera
 stopCameraBtn.addEventListener('click', () => {
     // console.log("Stop Camera");
     if (stream) {
@@ -29,6 +40,47 @@ stopCameraBtn.addEventListener('click', () => {
     disableBtn(stream);
 });
 
+// Take picture
+takePhotoBtn.addEventListener("click", () => {
+    if (!stream) return;
+
+    const ctx = canvas.getContext('2d');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const image = canvas.toDataURL("img/png");
+    imgPhoto.src = image;
+
+    imgPhoto.style.display = "block";
+    photoPlaceholder.style.display = 'none';
+
+    checkPhotoExist();
+});
+
+// Download picture
+downloadBtn.addEventListener('click', () => {
+    const link = document.createElement('a');
+    if (imgPhoto.src) {
+        link.download = 'photo.png';
+        link.href = canvas.toDataURL();
+        link.click();
+    } else {
+        alert("Không có ảnh để tải về");
+    }
+});
+
+// Kiểm tra xem có ảnh đã chụp không
+function checkPhotoExist() {
+    if (imgPhoto.src) {
+        downloadBtn.disabled = false;
+    } else {
+        downloadBtn.disabled = true;
+    }
+}
+
+// Hàm kiểm soát nút bấm
 function disableBtn(stream) {
     if (stream) {
         startCameraBtn.disabled = true;
