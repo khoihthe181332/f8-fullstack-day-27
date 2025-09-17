0475787bc23b1a37117263ab4299ce34571043d8
khoihthe181332 <hatuankhoi2004@gmail.com>
2025-09-17 22:10:25 +0700

    Nộp bài 2, chưa hoàn thiện event click F5

--- History_API_Navigation/main.js
+++ History_API_Navigation/main.js
@@ -1,42 +1,128 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const historyLength = $('#history-length');
-
-// Xử lý hiển thị history length
-historyLength.innerText = history.length;
+let currentPage = 'home';
 
 // Lấy phần tử trong DOM
-const navBtns = $$('.nav-link'); // Các nút điều hướng
+const navLinks = $$('.nav-link'); // Các nút điều hướng
 const pages = $$('.page'); // Các trang nội dung
 
-navBtns.forEach(btn => {
-    btn.addEventListener('click', function (e) {
-        const pageId = e.target.dataset.page;
-        pages.forEach(page => {
-            let currentPage = $('.page.active');
-            if (page.id === pageId) {
-                if (!page.classList.contains('active')) {
-                    // Ẩn trang hiện tại
-                    currentPage.classList.remove('active');
-                    // Hiển thị trang được chọn
-                    page.classList.add('active');
-                    // Khi chuyển URL sẽ k reload
-                    history.pushState({ page: pageId }, null, `${pageId}`);
-                }
-            }
-        });
+// Hàm lấy hiển thị page 
+function showPage(pageName) {
+    pages.forEach(page => {
+        page.classList.remove("active");
     });
-});
 
-window.addEventListener("popstate", () => {
-    console.log(123);
-});
-
-window.addEventListener("keyup", (e) => {
-    console.log(e);
-    if (e.key === 'F5') {
-        e.preventDefault();
-        console.log("F5");
+    const selectedPage = $(`#${pageName}`);
+    if (selectedPage) {
+        selectedPage.classList.add("active");
+        currentPage = pageName;
     }
+}
+
+// Hàm update class Active vào nút điều hướng
+function updateActiveClass(pageName) {
+    navLinks.forEach(item => {
+        item.classList.remove("active");
+    });
+
+    const activeLink = $(`[data-page="${pageName}"]`);
+    if (activeLink) {
+        activeLink.classList.add("active");
+    }
+}
+
+function handleMenuClick(e) {
+    e.preventDefault();
+    const pageName = e.target.dataset.page
+    if (pageName === currentPage) return;
+
+    const newURL = `/${pageName}`;
+    history.pushState({ page: pageName }, null, newURL);
+
+    showPage(pageName);
+    updateActiveClass(pageName);
+}
+
+// Hàm khởi tạo khi trang được load
+function initializePage() {
+    // Lấy trang hiện tại từ URL
+    let pageName = 'home';
+
+    const currentURL = window.location.pathname;
+    switch (currentURL) {
+        case '/about':
+            pageName = 'about';
+            break;
+        case '/services':
+            pageName = 'services';
+            break;
+        case 'contact':
+            pageName = 'contact';
+            break
+        default:
+            pageName = 'home';
+            break;
+    };
+
+    // Hiển thị trang tương ứng với URL
+    showPage(pageName);
+    updateActiveClass(pageName);
+}
+
+
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    e.preventDefault();
+
+    initializePage();
+
+    // Xử lý click menu 
+    navLinks.forEach(btn => {
+        btn.addEventListener("click", handleMenuClick);
+    });
+
+
+    window.addEventListener("popstate", (e) => {
+        let pageName = 'home';
+
+        if (e.state && e.state.page) {
+            pageName = e.state.page;
+        } else {
+            const currentURL = window.location.pathname;
+            switch (currentURL) {
+                case '/about':
+                    pageName = 'about';
+                    break;
+                case '/services':
+                    pageName = 'services';
+                    break;
+                case 'contact':
+                    pageName = 'contact';
+                    break
+                default:
+                    pageName = 'home';
+                    break;
+            };
+        }
+
+
+        showPage(pageName);
+        updateActiveClass(pageName);
+    });
+
+    // Xử lý nút F5
+    window.addEventListener("keyup", (e) => {
+        e.preventDefault();
+        if (e.key === 'F5') {
+            console.log("F5 clicked");
+        }
+    });
+
 });
+
+
+const historyLength = $('#history-length');
+historyLength.innerText = history.length; // Xử lý hiển thị history length
+
+
